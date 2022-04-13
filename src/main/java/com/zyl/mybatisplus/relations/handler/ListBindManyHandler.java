e7d73bf445496b014b0580e76f73f0105755c0a8
zhuyunlong <920200256@qq.com>
2022-04-13 15:35:16 +0800

    modified

--- src/main/java/com/zyl/mybatisplus/relations/handler/ListBindManyHandler.java
+++ src/main/java/com/zyl/mybatisplus/relations/handler/ListBindManyHandler.java
@@ -3,6 +3,7 @@ package com.zyl.mybatisplus.relations.handler;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.zyl.mybatisplus.relations.RelationCache;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 
@@ -19,8 +20,11 @@ public class ListBindManyHandler<T> extends ListHandler<T> {
     protected void queryRelation(RelationCache cache, LambdaQueryWrapper wrapper) {
         HashMap collect =
                 (HashMap) (getForeignModel(cache)).selectList(wrapper).stream().collect(groupingBy(cache.getForeignPropertyGetter()));
-        list.forEach(e -> cache.getRelationEntitySetter().accept(e,
-                collect.get(cache.getLocalPropertyGetter().apply(e))));
+        list.forEach(e -> {
+            Object list = collect.get(cache.getLocalPropertyGetter().apply(e));
+            cache.getRelationEntitySetter()
+                    .accept(e, null == list ? Collections.emptyList() : list);
+        });
     }
 
 }
